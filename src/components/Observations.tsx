0064032ba44b4cfed2dc471c7ecf930862794618
Jerome Wuerf <oss@scharom.de>
2023-08-09 15:08:21 +0200

    Update observation list

--- src/components/Observations.tsx
+++ src/components/Observations.tsx
@@ -1,9 +1,35 @@
 import React from 'react'
+import { api } from "~/utils/api";
+import LoadingSpinner from "./LoadingSpinner";
+import { Table } from "react-daisyui";
 
 type Props = {selectedSport: string}
 
 export default function Observations({selectedSport}: Props) {
-  return (
-    <div>Observe</div>
-  )
+  const getObservationsQuery = api.observation.getObservations.useQuery({
+    selectedSport
+  })
+  return getObservationsQuery.data ? (
+    <Table zebra>
+      <Table.Head>
+        <span />
+        <span>lon</span>
+        <span>lat</span>
+      </Table.Head>
+      <Table.Body>
+        {getObservationsQuery.data.map(
+          ({ sportPlace }, idx) =>
+            sportPlace && (
+              <Table.Row>
+                <span>{idx}</span>
+                <span>{sportPlace.lon}</span>
+                <span>{sportPlace.lat}</span>
+              </Table.Row>
+            )
+        )}
+      </Table.Body>
+    </Table>
+  ) : (
+    <LoadingSpinner />
+  );
 }
--- src/server/api/routers/observation.ts
+++ src/server/api/routers/observation.ts
@@ -1,7 +1,28 @@
+import { Sport } from "@prisma/client";
 import { z } from "zod";
 import { createTRPCRouter, protectedProcedure } from "~/server/api/trpc";
 
 export const observationRouter = createTRPCRouter({
+  getObservations: protectedProcedure
+    .input(z.object({ selectedSport: z.string() }))
+    .query(({ ctx, input }) => {
+      return ctx.prisma.observation.findMany({
+        where: {
+          userId: ctx.session.user.id,
+          sportPlace: {
+            type: input.selectedSport as Sport,
+          },
+        },
+        select: {
+          sportPlace: {
+            select: {
+              lat: true,
+              lon: true,
+            },
+          },
+        },
+      });
+    }),
   createObservation: protectedProcedure
     .input(z.object({ sportPlaceId: z.string().cuid() }))
     .mutation(async ({ ctx, input }) => {
